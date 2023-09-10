1fc69c25f00637e4b9ffde878b7daf47283a4006
doom9star <karthikvasudevan18@gmail.com>
2023-09-10 23:31:46 +0530

    refined activate account flow!

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import bcrypt from "bcryptjs";
 import User from "../entities/User";
 import {
-  ACTIVATE_PASSWORD_PREFIX,
+  ACTIVATE_ACCOUNT_PREFIX,
   APP_PREFIX,
   COOKIE_NAME,
   FORGOT_PASSWORD_PREFIX,
@@ -27,30 +27,39 @@ router.get("/", isAuth, async (req: TAuthRequest, res) => {
   }
 });
 
-router.post("/activate/:tid", isAuth, async (req: TAuthRequest, res) => {
-  try {
-    const { tid } = req.params;
-    const uid = req.redclient.get(
-      `${APP_PREFIX}${ACTIVATE_PASSWORD_PREFIX}${tid}`
-    );
+router.post(
+  "/activate-account/:tid",
+  isAuth,
+  async (req: TAuthRequest, res) => {
+    try {
+      const { tid } = req.params;
+      const { password } = req.body;
 
-    const user = await User.findOne({ where: { id: uid } });
-    if (!user) return res.json(getResponse(404));
+      const uid = req.redclient.get(
+        `${APP_PREFIX}${ACTIVATE_ACCOUNT_PREFIX}${tid}`
+      );
 
-    if (user.activated)
-      return res.json(getResponse(400, "Account already activated!"));
+      const user = await User.findOne({ where: { id: uid } });
+      if (!user) return res.json(getResponse(404));
 
-    user.activated = true;
-    await user.save();
+      if (user.activated)
+        return res.json(getResponse(400, "Account already activated!"));
 
-    req.redclient.del(`${APP_PREFIX}${ACTIVATE_PASSWORD_PREFIX}${tid}`);
+      const authorized = await user.checkPassword(password);
+      if (!authorized) return res.json(getResponse(401));
 
-    return res.json(getResponse(200));
-  } catch (error: any) {
-    log("ERROR", error.message);
-    return res.json(getResponse(500, error.message));
+      user.activated = true;
+      await user.save();
+
+      req.redclient.del(`${APP_PREFIX}${ACTIVATE_ACCOUNT_PREFIX}${tid}`);
+
+      return res.json(getResponse(200));
+    } catch (error: any) {
+      log("ERROR", error.message);
+      return res.json(getResponse(500, error.message));
+    }
   }
-});
+);
 
 router.post("/register", isNotAuth, async (req, res) => {
   try {
